77d49968c7e8340bbd51c79e4a4524a73179d0ad
ysyx_23060030-Yuan Jiahao <yjh2477664364@gmail.com>
2024-05-07 06:35:17 +0800

    0到1的伟大突破

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use http_server::*;
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(8);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    OK(())
+}
