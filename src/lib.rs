0de36446d971d5816ea91b88270b08cf7f7e0632
ysyx_23060030-Yuan Jiahao <yjh2477664364@gmail.com>
2024-05-13 22:14:20 +0800

    basic

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 pub use clap::Parser;
 use regex::Regex;
+use serde_json::json;
 use std::{
+    collections::HashMap,
     error::Error,
     fs,
     io::prelude::*,
     net::TcpStream,
+    net::ToSocketAddrs,
     sync::{mpsc, Arc, Mutex},
     thread,
 };
@@ -36,13 +39,6 @@ pub struct ThreadPool {
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
@@ -78,8 +74,6 @@ impl Drop for ThreadPool {
         drop(self.sender.take());
 
         for worker in &mut self.workers {
-            println!("Shutting down worker {}", worker.id);
-
             if let Some(thread) = worker.thread.take() {
                 thread.join().unwrap();
             }
@@ -88,30 +82,27 @@ impl Drop for ThreadPool {
 }
 
 struct Worker {
-    id: usize,
+    _id: usize,
     thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(_id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
             let message = receiver.lock().unwrap().recv();
 
             match message {
                 Ok(job) => {
-                    println!("Worker {id} got a job; executing.");
-
                     job();
                 }
                 Err(_) => {
-                    println!("Worker {id} disconnected; shutting down.");
                     break;
                 }
             }
         });
 
         Worker {
-            id,
+            _id,
             thread: Some(thread),
         }
     }
@@ -121,54 +112,47 @@ pub fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer)?;
 
-    let re = Regex::new("id=[0-9]+&name=[a-zA-Z0-9]+")?;
-
     let (method, path, _) = parse_request(&buffer)?;
 
-    let (status_line, content_type, contents) = match (method.as_str(), path.as_str()) {
-        ("GET", "/") | ("GET", "/index.html") => {
-            let contents = fs::read_to_string("static/index.html")?;
-            ("HTTP/1.1 200 OK", "text/html", contents)
-        }
-        ("GET", "/501.html") => {
-            let contents = fs::read_to_string("static/501.html")?;
-            ("HTTP/1.1 501 OK", "text/html", contents)
-        }
-        ("GET", "/api/check") => {
-            let contents = fs::read_to_string("data/data.txt")?;
-            ("HTTP/1.1 200 OK", "text/html", contents)
-        }
-        ("POST", "/api/echo") => {
-            let body_string = String::from_utf8_lossy(&buffer).to_string();
-            let body: Vec<&str> = body_string.split("\r\n\r\n").collect();
-
-            let data = if let Some(body) = body.get(1) {
-                *body
-            } else {
-                return Err("No body found in the request".into());
-            };
-            match re.is_match(data) {
-                true => {
-                    let contents = format!("{}", "id=1&name=Foo");
-                    (
-                        "HTTP/1.1 200 OK",
-                        "application/x-www-form-urlencoded",
-                        contents,
-                    )
-                }
-                false => (
-                    "HTTP/1.1 404 OK",
-                    "text/plain",
-                    fs::read_to_string("data/error.txt")?,
-                ),
+    let (status_line, content_type, contents) = if method != "GET" || method != "POST" {
+        let contents = fs::read_to_string("static/501.html")?;
+        ("HTTP/1.1 501 OK", "text/html", contents)
+    } else {
+        match (method.as_str(), path.as_str()) {
+            ("GET", "/") | ("GET", "/index.html") => read_static_file("static/index.html")?,
+            ("GET", "/501.html") => {
+                let contents = fs::read_to_string("static/501.html")?;
+                (
+                    "HTTP/1.1 501 Not Implemented",
+                    detect_content_type("static/404.html"),
+                    contents,
+                )
+            }
+            ("GET", "/api/check") => read_static_file("data/data.txt")?,
+            ("GET", "/api/list") => read_static_file("data/data.json")?,
+            ("POST", "/api/echo") => handle_echo_request(&buffer)?,
+            ("POST", "/api/upload") => handle_upload_request(&buffer)?,
+            ("GET", path) if path.starts_with("/api/search") => handle_search_request(path)?,
+            ("GET", path) if path.ends_with(".html") => {
+                read_static_file(&format!("static{}", path))?
+            }
+            ("GET", path) if path.ends_with(".js") => read_static_file(&format!("static{}", path))?,
+            ("GET", path) if path.ends_with(".json") => {
+                read_static_file(&format!("static{}", path))?
+            }
+            ("GET", path) if path.ends_with(".css") => {
+                read_static_file(&format!("static{}", path))?
+            }
+            _ => {
+                let contents = fs::read_to_string("static/404.html")?;
+                (
+                    "HTTP/1.1 404 NOT FOUND",
+                    detect_content_type("static/404.html"),
+                    contents,
+                )
             }
         }
-        _ => {
-            let contents = fs::read_to_string("static/404.html")?;
-            ("HTTP/1.1 404 NOT FOUND", "text/html", contents)
-        }
     };
-
     let response = format!(
         "{}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
         status_line,
@@ -179,6 +163,7 @@ pub fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
 
     stream.write_all(response.as_bytes())?;
     stream.flush()?;
+
     Ok(())
 }
 
@@ -189,9 +174,213 @@ fn parse_request(buffer: &[u8]) -> Result<(String, String, String), Box<dyn Erro
     let first_line = lines.next().ok_or("Empty request")?;
     let mut parts = first_line.split_whitespace();
 
-    let method = parts.next().ok_or("Invalid request")?.to_string();
-    let path = parts.next().ok_or("Invalid request")?.to_string();
-    let protocol = parts.next().ok_or("Invalid request")?.to_string();
+    let method = parts.next().ok_or("Invalid method")?.to_string();
+    let path = parts.next().ok_or("Invalid path")?.to_string();
+    let protocol = parts.next().ok_or("Invalid protocol")?.to_string();
 
     Ok((method, path, protocol))
+}
+
+fn read_static_file(path: &str) -> Result<(&'static str, &'static str, String), Box<dyn Error>> {
+    let contents = fs::read_to_string(path)?;
+    Ok(("HTTP/1.1 200 OK", detect_content_type(path), contents))
+}
+
+fn detect_content_type(path: &str) -> &'static str {
+    match path {
+        _ if path.ends_with(".html") => "text/html",
+        _ if path.ends_with(".js") => "text/javascript",
+        _ if path.ends_with(".json") => "application/json",
+        _ if path.ends_with(".css") => "text/css",
+        _ => "text/plain",
+    }
+}
+
+fn handle_echo_request(
+    buffer: &[u8],
+) -> Result<(&'static str, &'static str, String), Box<dyn Error>> {
+    let body_string: String = String::from_utf8_lossy(buffer).to_string();
+    let body: Vec<&str> = body_string.trim_matches('\0').split("\r\n\r\n").collect();
+
+    let data = if let Some(body) = body.get(1) {
+        *body
+    } else {
+        return Err("No body found in the request".into());
+    };
+
+    let re = Regex::new("id=[0-9]+&name=[a-zA-Z0-9]+")?;
+
+    match re.is_match(data) {
+        true => Ok((
+            "HTTP/1.1 200 OK",
+            "application/x-www-form-urlencoded",
+            format!("{data}"),
+        )),
+        false => Ok((
+            "HTTP/1.1 403 Data format error",
+            "text/plain",
+            fs::read_to_string("data/error.txt")?,
+        )),
+    }
+}
+
+fn handle_upload_request(
+    buffer: &[u8],
+) -> Result<(&'static str, &'static str, String), Box<dyn Error>> {
+    let content_type = extract_content_type(&buffer)?;
+
+    let content_type = content_type.as_str();
+
+    match content_type {
+        "application/json" => {
+            let body_string: String = String::from_utf8_lossy(&buffer).to_string();
+            let body: Vec<&str> = body_string.trim_matches('\0').split("\r\n\r\n").collect();
+            let data = if let Some(body) = body.get(1) {
+                *body
+            } else {
+                return Err("No body found in the request".into());
+            };
+
+            Ok(("HTTP/1.1 200 OK", "application/json", data.to_string()))
+        }
+        "application/x-www-form-urlencoded" => {
+            let body_string: String = String::from_utf8_lossy(&buffer).to_string();
+            let body_parts: Vec<&str> = body_string.trim_matches('\0').split("\r\n\r\n").collect();
+
+            let data = if let Some(body) = body_parts.get(1) {
+                *body
+            } else {
+                return Err("No body found in the request".into());
+            };
+
+            let re = Regex::new(r"id=\d+&name=[a-zA-Z0-9]+")?;
+
+            if let Some(captures) = re.captures(data) {
+                let id = captures.get(1).map_or("", |m| m.as_str());
+                let name = captures.get(2).map_or("", |m| m.as_str());
+
+                let response = json!({
+                    "id": id,
+                    "name": name
+                });
+
+                Ok(("HTTP/1.1 200 OK", "application/json", response.to_string()))
+            } else {
+                let response = fs::read_to_string("data/error.json")?;
+                Ok((
+                    "HTTP/1.1 403 Data format error",
+                    "application/json",
+                    response,
+                ))
+            }
+        }
+
+        _ => Ok((
+            "HTTP/1.1 404 NOT FOUND",
+            "text/html",
+            fs::read_to_string("static/404.html")?,
+        )),
+    }
+}
+
+fn handle_search_request(
+    path: &str,
+) -> Result<(&'static str, &'static str, String), Box<dyn Error>> {
+    let path_parts: Vec<&str> = path.split('?').collect();
+
+    let query_params = if path_parts.len() == 2 {
+        parse_query_params(path_parts[1])?
+    } else {
+        HashMap::new()
+    };
+
+    let id = query_params.get("id").map_or("", |id| id.trim());
+    let name = query_params.get("name").map_or("", |name| name.trim());
+
+    let data = fs::read_to_string("data/data.json")?;
+    let json_data: serde_json::Value = serde_json::from_str(&data)?;
+
+    let matching_objects: Vec<_> = json_data
+        .as_array()
+        .ok_or("JSON data is not an array")?
+        .iter()
+        .filter(|obj| {
+            obj.get("id")
+                .and_then(|id_val| id_val.as_u64())
+                .map_or(false, |id_val| id_val == id.parse::<u64>().unwrap())
+                && obj
+                    .get("name")
+                    .and_then(|name_val| name_val.as_str())
+                    .map_or(false, |name_val| name_val == name)
+        })
+        .collect();
+
+    if !matching_objects.is_empty() {
+        let response = serde_json::to_string(&matching_objects)?;
+        Ok(("HTTP/1.1 200 OK", "application/json", response))
+    } else {
+        let response = fs::read_to_string("data/not_found.json")?;
+        Ok(("HTTP/1.1 404 NOT FOUND", "application/json", response))
+    }
+}
+
+fn extract_content_type(buffer: &[u8]) -> Result<String, Box<dyn Error>> {
+    let request = String::from_utf8_lossy(buffer);
+    let headers: Vec<&str> = request.trim_matches('\0').split("\r\n").collect();
+
+    let content_type_line = headers.get(4).ok_or("No Content-Type header found")?;
+    let content_type_parts: Vec<&str> = content_type_line.split(":").collect();
+
+    if content_type_parts.len() != 2 || content_type_parts[0].trim() != "Content-Type" {
+        return Err("Invalid Content-Type header".into());
+    }
+
+    Ok(content_type_parts[1].trim().to_string())
+}
+
+fn parse_query_params(query: &str) -> Result<HashMap<String, String>, String> {
+    let mut params = HashMap::new();
+
+    for param in query.split('&') {
+        let parts: Vec<&str> = param.splitn(2, '=').collect();
+        if parts.len() != 2 {
+            return Err("Invalid query parameter format".to_string());
+        }
+        params.insert(parts[0].to_string(), parts[1].to_string());
+    }
+
+    Ok(params)
+}
+
+fn _proxy_request(
+    mut _client_stream: TcpStream,
+    _proxy_address: String,
+) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+pub fn extract_proxy_address(proxy: &str) -> Result<String, Box<dyn Error>> {
+    let re = Regex::new(r"http://([^:/]+):?(\d+)?").unwrap();
+
+    if let Some(captures) = re.captures(proxy) {
+        let address = captures.get(1).unwrap().as_str();
+        let port_str = captures.get(2).map_or("80", |port| port.as_str());
+
+        let port: u16 = match port_str.parse() {
+            Ok(port) => port,
+            Err(_) => return Err("Failed to parse port".into()),
+        };
+
+        let addr = match (address, port).to_socket_addrs() {
+            Ok(mut addr_iter) => match addr_iter.next() {
+                Some(addr) => addr,
+                None => return Err("Failed to resolve address".into()),
+            },
+            Err(e) => return Err(format!("Socket address resolution error: {}", e).into()),
+        };
+
+        Ok(addr.to_string())
+    } else {
+        Err("Invalid proxy address format".into())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,47 @@
 use http_server::*;
 use std::{net::TcpListener, process::exit};
+
 fn main() {
     let args = Args::parse();
 
-    let listener =
-        TcpListener::bind((args.ip + ":" + args.port.as_str()).as_str()).unwrap_or_else(|err| {
-            eprintln!("{err}");
-            exit(1)
-        });
+    let listener = TcpListener::bind(format!("{}:{}", args.ip, args.port))
+        .unwrap_or_else(|err| exit_with_error(&format!("{}", err)));
 
     let pool = ThreadPool::new(args.threads as usize);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap_or_else(|err| {
-            eprintln!("{err}");
-            exit(1)
-        });
+    //let proxy_enabled = !args.proxy.is_empty();
 
-        pool.execute(|| {
-            handle_connection(stream).unwrap_or_else(|err| {
-                eprintln!("{err}");
-                exit(1)
-            })
-        })
-        .unwrap_or_else(|err| {
-            eprintln!("{err}");
-            exit(1)
-        });
+    // let proxy_address = if proxy_enabled {
+    //     match extract_proxy_address(&args.proxy) {
+    //         Ok(proxy_address) => proxy_address,
+    //         Err(err) => exit_with_error(&format!("{}", err)),
+    //     }
+    // } else {
+    //     String::new()
+    // };
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                //let proxy_address_clone = proxy_address.clone();
+
+                pool.execute(move || {
+                    handle_connection(stream).unwrap_or_else(|err| {
+                        exit_with_error(&format!("{}", err));
+                    })
+                })
+                .unwrap_or_else(|err| {
+                    exit_with_error(&format!("{}", err));
+                });
+            }
+            Err(err) => {
+                eprintln!("{}", err);
+            }
+        }
     }
+}
+
+fn exit_with_error(msg: &str) -> ! {
+    eprintln!("{}", msg);
+    exit(1);
 }
